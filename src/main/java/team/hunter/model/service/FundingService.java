e3a1ba15eadfc48db75c29f5296edfb3ce8e2215
jinyuo <aleydis95@gmail.com>
2020-01-03 16:52:25 +0900

    commit for dto setting

--- src/main/java/team/hunter/model/service/FundingService.java
+++ src/main/java/team/hunter/model/service/FundingService.java
@@ -1,8 +1,12 @@
 package team.hunter.model.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import team.hunter.model.dto.Funding;
 
 @Service
 public interface FundingService {
-
+	public List<Funding> selectAll();
 }
