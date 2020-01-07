21f6c91d0f5fa04640fb55fd9f94f8bd7ad463c0
sunyeongchoi <sn0716@naver.com>
2020-01-07 18:11:36 +0900

    sn findpwd

---
+++ src/main/java/team/hunter/controller/EmailController.java
@@ -0,0 +1,53 @@
+package team.hunter.controller;
+
+import java.util.Random;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import team.hunter.model.dto.Member;
+import team.hunter.model.email.Email;
+import team.hunter.model.email.EmailSender;
+import team.hunter.model.service.MemberService;
+
+@Controller
+public class EmailController {
+	@Autowired private EmailSender emailSender;
+	@Autowired private MemberService memberService;
+	@Autowired private Email email;
+
+	// 새로운 비밀번호가 생성된다.
+	@RequestMapping("/pwdCheck")
+	public String newPassword(Member member, HttpSession session) throws Exception {
+		System.out.println("member나오니???"+member.getPhone()+member.getName()+member.getEmail());
+		Random r = new Random();
+		int num = r.nextInt(89999) + 10000;
+		String npassword = "bapsi" + Integer.toString(num);// 새로운 비밀번호 변경
+		member.setPwd(npassword);
+		session.setAttribute("member", member);
+		return "forward:findPassword";
+	}
+
+
+
+	// 이메일로 비밀번호가 전송이된다.
+	@RequestMapping("/findPassword")
+	public String findPasswordOK(Member member, HttpSession session) throws Exception {
+		member = (Member) session.getAttribute("member");
+			email.setContent("새로운 비밀번호 " + member.getPwd() + " 입니다." );
+			email.setReceiver(member.getEmail());
+			email.setSubject("안녕하세요"+member.getEmail() +"님  재설정된 비밀번호를 확인해주세요");
+			emailSender.SendEmail(email);
+			System.out.println(email);
+			memberService.newPassword(member);
+			session.invalidate();
+			return "redirect:login";
+	}
+	
+}
