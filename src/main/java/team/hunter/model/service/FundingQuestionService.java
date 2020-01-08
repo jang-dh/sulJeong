d4839ebedca5c52be8ca2707d96e6eca9d127c12
jinyuo <aleydis95@gmail.com>
2020-01-08 14:22:24 +0900

    fix 2nd merge error

--- src/main/java/team/hunter/model/service/FundingQuestionService.java
+++ src/main/java/team/hunter/model/service/FundingQuestionService.java
@@ -10,7 +10,6 @@ import team.hunter.model.dto.FundingRequest;
 public interface FundingQuestionService {
   int insert(FundingQuestion fundingQuestion); 
   
-	int questionInsert(FundingRequest fundingRequest); 
 
 	List<FundingQuestion> fundingQuestionList(int memberCode);
 	
