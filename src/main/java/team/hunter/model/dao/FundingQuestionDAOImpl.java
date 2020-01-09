f2158b314d6d87bae0df6330a1e9e609b6f7d189
jang-dh <jangdh0428@gmail.com>
2020-01-09 19:48:33 +0900

    login and paging

--- src/main/java/team/hunter/model/dao/FundingQuestionDAOImpl.java
+++ src/main/java/team/hunter/model/dao/FundingQuestionDAOImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import team.hunter.model.dto.FundingQuestion;
-import team.hunter.model.dto.FundingRequest;
+import team.hunter.model.dto.Member;
 
 @Repository
 public class FundingQuestionDAOImpl implements FundingQuestionDAO {
@@ -21,13 +21,17 @@ public class FundingQuestionDAOImpl implements FundingQuestionDAO {
 		return result;
 	}
 	@Override
-	public List<FundingQuestion> fundingQuestionList(int memberCode) {
-		return session.selectList("fundingQuestionMapper.fundingQuestionList", memberCode);
+	public List<FundingQuestion> fundingQuestionList(Member member) {
+		return session.selectList("fundingQuestionMapper.fundingQuestionList", member);
 	}
 	@Override
 	public FundingQuestion selectByCode(int code) {
 		FundingQuestion fundingQuestion = session.selectOne("fundingQuestionMapper.selectByCode", code);
 		return fundingQuestion;
+	}
+	@Override
+	public int listCount(int memberCode) {
+		return session.selectOne("fundingQuestionMapper.listCount",memberCode);
 	}
 
 }
