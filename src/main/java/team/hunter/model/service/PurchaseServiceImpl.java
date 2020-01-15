2e4a971209239e38ed99cbc58f3d6f6cc4e3e251
jinyuo <aleydis95@gmail.com>
2020-01-15 12:03:46 +0900

    fix schedule

--- src/main/java/team/hunter/model/service/PurchaseServiceImpl.java
+++ src/main/java/team/hunter/model/service/PurchaseServiceImpl.java
@@ -1,16 +1,20 @@
 package team.hunter.model.service;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.request.CancelData;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+
 import team.hunter.model.dao.FundingDAO;
 import team.hunter.model.dao.PurchaseDAO;
-import team.hunter.model.dto.Funding;
-import team.hunter.model.dto.FundingRequest;
 import team.hunter.model.dto.Member;
 import team.hunter.model.dto.Purchase;
 
@@ -22,7 +26,7 @@ public class PurchaseServiceImpl implements PurchaseService {
 
 	@Autowired
 	private FundingDAO fundingDAO;
-
+	
 	@Override
 	@Transactional
 	public int insert(Purchase purchase) {
@@ -85,5 +89,40 @@ public class PurchaseServiceImpl implements PurchaseService {
 		System.out.println("서비스를 가느냐?");
 		System.out.println("서비스를 나오느냐?");
 		return purchaseDAO.purchaseList(memberCode, startIndex, cntPerPage);
+	}
+	
+	public void cancelPurchase(Purchase purchase) {
+		IamportClient iamportClient = new IamportClient("9641301071926320",
+				"DGvvhuqgbRnvUxwBIwOoU5tDk5AH28ZGPvb7ZCnbtLHnjdZ1JOpETTieYSW11WIRrTYrvmCZ7jnqxnrh");
+		CancelData cancelData = new CancelData(purchase.getMerchantUid(), false);
+
+		try {
+			IamportResponse<Payment> response = iamportClient.cancelPaymentByImpUid(cancelData);
+			System.out.println("response.getCode() :" + response.getCode());
+			System.out.println("response.getMessage() :" + response.getMessage());
+			// System.out.println("response.getResponse().getAmount() :" +
+			// response.getResponse().getAmount());
+			// TODO : 처리 로직
+		} catch (IamportResponseException e) {
+			System.out.println(e.getMessage());
+
+			switch (e.getHttpStatusCode()) {
+			case 401:
+				// TODO : 401 Unauthorized
+				System.out.println(e.getMessage());
+				break;
+			case 404:
+				// TODO : imp_123412341234 에 해당되는 거래내역이 존재하지 않음
+				System.out.println(e.getMessage());
+				break;
+			case 500:
+				// TODO : 서버 응답 오류
+				System.out.println(e.getMessage());
+				break;
+			}
+		} catch (IOException e) {
+			// 서버 연결 실패
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/team/hunter/scheduler/TaskService.java
+++ src/main/java/team/hunter/scheduler/TaskService.java
@@ -1,56 +1,59 @@
 package team.hunter.scheduler;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-<<<<<<< HEAD
-import team.hunter.model.dto.Funding;
-=======
+import team.hunter.model.dto.Purchase;
+import team.hunter.model.service.PurchaseServiceImpl;
 import team.hunter.model.dao.FundingDAO;
-import team.hunter.model.dao.StatisticsDAO;
->>>>>>> origin/funding
-import team.hunter.model.service.FundingService;
-import team.hunter.model.service.NoticeService;
-import team.hunter.model.service.PurchaseService;
+import team.hunter.model.dao.NoticeDAO;
+import team.hunter.model.dao.PurchaseDAO;
 
 @Service
 public class TaskService {
 	@Autowired
-	private FundingService fundingService;
-	
-	@Autowired
-	private NoticeService noticeService;
-	
-	@Autowired
-	private PurchaseService purchaseService;
-	
-	@Autowired
 	private FundingDAO fundingDAO;
 	
 	@Autowired
-	private StatisticsDAO statisticsDAO;
+	private NoticeDAO noticeDAO;
+	
+	@Autowired
+	private PurchaseDAO purchaseDAO;
+	
+	@Autowired PurchaseServiceImpl purchaseServiceImpl;
 	
 	//@Scheduled(cron = "0 0/30 0 * * *")
-	@Scheduled(cron="1 20 09 * * *")//매일 0시 0분 1초에 실행
-	public void TestScheduler() {
-		System.out.println("call updateFunding...");
-		fundingService.updateFundingState();
+	@Scheduled(cron="1 0 0 * * *")//매일 0시 0분 1초에 실행
+	public void scheduler() {
+		updateDB();
+		
+		//취소할 결제 목록
+		List<Purchase> canceledList = purchaseDAO.selectCanceled();
+		
+		//결제 취소
+		for(Purchase purchase : canceledList) {
+			purchaseServiceImpl.cancelPurchase(purchase);
+		}
+			
 	}
 	
-	@Scheduled(cron = "1 25 09 * * * ")
-	public void purchaseStateUpdateScheduler() {
-		System.out.println("call purchaseStateUpdateScheduler...");
-		purchaseService.updatePurchaseState();		
+	@Transactional
+	public void updateDB() {
+		//자정이 지나면
+		System.out.println("call scheduler");
+		//statistics 테이블에 레코드 추가
+		noticeDAO.insertStatistics();
+		
+		//펀딩 마감 및 오픈
+		fundingDAO.updateFundingStateClose();
+		fundingDAO.updateFundingStateOpen();
+		
+		//구매 상태 변경
+		purchaseDAO.updatePurchaseState();
 	}
-	
-	@Scheduled(cron = "0 30 09 * * * ")
-	public void statisticsScheduler() {
-		System.out.println("call statisticsScheduler....");
-		noticeService.insertStatistics();
-	}
+
 }
